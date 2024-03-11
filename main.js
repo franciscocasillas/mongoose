eae0f4968b087e27b5f285f9df1cc9ed09e97c90
Francisco J. Casillas <chiccasillas@gmail.com>
2024-03-11 11:42:15 -0600

    Create first model and schema

---
+++ main.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+mongoose.connect(
+	"mongodb+srv://lucanocasillas:sj17QfwUYUGmXaMg@cluster0.enyrnwg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+);
